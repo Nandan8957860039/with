f2aba6105e2034f259a24378516ba2ba90a2f632
nandan8957860039 <sanatanbajpai@gmail.com>
2024-09-11 00:50:57 +0530

    footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>home</li>
+        <li>About</li>
+        <li>contact us</li>
+        <li>mobile no.</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
